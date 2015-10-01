5b04d27d92d2481a8717cea590794ce02243800e
vladych <vlad@vladych.nonexist.com>
2015-10-02 00:13:52 +0200

    added some run script

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,11 +1,18 @@
 from model.DAO import DAO, DAOList
+import os
+import logging
+from helpers.config_helper import discover_config, read_config
+from helpers.CONST import CONST
 
 if __name__=="__main__":
-	d=DAO()
-	dl=DAOList()
-	dl.add(d)
-	try:
-		dl.add(None)
-	except BaseException as exc:
-		print(exc)
-	dl.add(d)
+    logging.basicConfig(filename=CONST.LOGGER_FILE_NAME, level=logging.DEBUG)
+    config = read_config(__file__)
+    logging.warn(config.getint('S','a'))
+    d=DAO(None)
+    dl=DAOList(None,DAO(None))
+    dl.add(d)
+    try:
+        dl.add(None)
+    except BaseException as exc:
+        print(exc)
+    dl.add(d)
