d14ffe3e59315173787bd9f4264e5127b7d7394d
Valerij Klein <valerij.klein@googlemail.com>
2015-10-09 09:48:04 +0200

    testrun for db connect

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -4,13 +4,14 @@ import os
 import logging
 from helpers.config_helper import discover_config, read_config
 from helpers.CONST import CONST
+from helpers import config
 
 if __name__=="__main__":
     logging.basicConfig(filename=CONST.LOGGER_FILE_NAME, level=logging.DEBUG)
-    config = read_config(__file__)
     logging.warn(config.getint('S','a'))
-    d=DAO(None)
-    dl=DAOList(DAO(None))
+    print(config.getint('S','a'))
+    d=DAO()
+    dl=DAOList(DAO())
     dl.add(d)
     try:
         dl.add(None)
@@ -18,11 +19,11 @@ if __name__=="__main__":
         print(exc)
     dl.add(d)
     a=ADAO()
-    adao_list=ADAOList()
+    adao_list=DAOList(a)
     adao_list.add(a)
     try:
         adao_list.add(d)
     except BaseException as exc:
         print(exc)
     dl.add(a)
-    adao_list.load_all()
+    print(adao_list.load())
