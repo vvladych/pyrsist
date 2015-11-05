501c7f0523ad78a6d9ab1f49b5a599d6f6141c63
vladych <vlad@vladych.nonexist.com>
2015-11-06 00:08:13 +0100

    added with statement for dbcursor

--- sandbox/model/DAO.py
+++ sandbox/model/DAO.py
@@ -2,6 +2,40 @@ import uuid
 import psycopg2.extras
 from sandbox.helpers.db_connection import get_db_connection
 
+
+def typecheck(func):
+    def func_wrapper(*args,**kwargs):
+        if not isinstance(args[1], args[0].dao_class):
+            raise BaseException("cannot add, type doesn't match %s %s" % (args[0], args[0].dao_class.__name__))
+        return func(*args)
+    return func_wrapper
+    
+def consistcheck(s=None):
+    def wrap(f):
+        def func_wrapper(*args):            
+            if not hasattr(args[0].dao,"entity") or args[0].dao.entity==None:
+                raise NotImplementedError("entity for dao %s is None" % args[0].dao_class)
+            if not s in args[0].sql_dict or args[0].sql_dict[s]==None:
+                raise NotImplementedError("%s in sql_dict not available" % s)
+            return f(*args)
+        return func_wrapper
+    return wrap
+    
+    
+class dbcursor_wrapper:
+    def __init__(self, query):
+        self.query=query
+
+    def __enter__(self):
+        self.cursor = get_db_connection().cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+        self.cursor.execute(self.query)
+        return self.cursor
+        
+    def __exit__(self, type, value, traceback):
+        self.cursor.close()
+
+        
+
 class DAO(object):
     
     entity=None
@@ -13,7 +47,6 @@ class DAO(object):
     
     def __init__(self):
         self.__uuid=uuid.uuid4()
-        #self.data_fields=["uuid"]
         self.sql_dict={}
         for p in self.data_fields:
             setattr(self, p, None)
@@ -57,46 +90,19 @@ class DAO(object):
     def delete(self):
         pass
         
-
-
-def typecheck(func):
-    def func_wrapper(*args,**kwargs):
-        if not isinstance(args[1], args[0].dao_class):
-            raise BaseException("cannot add, type doesn't match %s %s" % (args[0], args[0].dao_class.__name__))
-        return func(*args)
-    return func_wrapper
     
-def consistcheck(s=None):
-    def wrap(f):
-        def func_wrapper(*args):            
-            if not hasattr(args[0].dao,"entity") or args[0].dao.entity==None:
-                raise NotImplementedError("entity for dao %s is None" % args[0].dao_class)
-            if not s in args[0].sql_dict or args[0].sql_dict[s]==None:
-                raise NotImplementedError("%s in sql_dict not available" % s)
-            return f(*args)
-        return func_wrapper
-    return wrap
-    
-def dbcursor(s=None):
-    def wrap(f):
-        def func_wrapper(*args):            
-            if not hasattr(args[0].dao,"entity") or args[0].dao.entity==None:
-                raise NotImplementedError("entity for dao %s is None" % args[0].dao_class)
-            if not s in args[0].sql_dict or args[0].sql_dict[s]==None:
-                raise NotImplementedError("%s in sql_dict not available" % s)
-            return f(*args)
-        return func_wrapper
-    return wrap            
     
 class DAOList(set):
 
     __LOAD_LIST_SQL_KEY_NAME="load"
         
+    sql_dict={__LOAD_LIST_SQL_KEY_NAME:"SELECT %s FROM %s;"}    
+        
     def __init__(self, DAO):
         super(DAOList, self).__init__()
         self.dao=DAO
         self.dao_class=DAO.__class__        
-        self.sql_dict={DAOList.__LOAD_LIST_SQL_KEY_NAME:"SELECT %s FROM %s;"}
+        
 
     @typecheck
     def add(self, DAO):
@@ -108,11 +114,10 @@ class DAOList(set):
                 
 
     @consistcheck("load")
-    def load(self, limit=None, orderby=None):
-        print(self.sql_dict[self.__LOAD_LIST_SQL_KEY_NAME] % (",".join(self.dao.data_fields), self.dao.entity))
-        cursor = get_db_connection().cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
-        cursor.execute(self.sql_dict[self.__LOAD_LIST_SQL_KEY_NAME] % (",".join(self.dao.data_fields), self.dao.entity))
-        rows=cursor.fetchall()
-        for row in rows:
-            self.add(self.dao_class.fabric_method(row))
-        cursor.close()
+    def load(self):
+        query=self.sql_dict[DAOList.__LOAD_LIST_SQL_KEY_NAME] % (",".join(self.dao.data_fields), self.dao.entity)
+        with dbcursor_wrapper(query) as cursor:            
+            rows=cursor.fetchall()
+            for row in rows:
+                self.add(self.dao_class.fabric_method(row))
+        
