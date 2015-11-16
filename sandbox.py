77139fb2f602567c7fa7417a9c39cd8e6bbd6f8d
vladych <vlad@vladych.nonexist.com>
2015-11-16 23:56:32 +0100

    daotodao as poc added

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,6 @@
 from sandbox.model.DAO import DAO, DAOList
 from sandbox.model.ADAO import ADAO
+from sandbox.model.BDAO import BDAO
 import os
 import logging
 from sandbox.helpers.config_helper import discover_config, read_config
@@ -12,12 +13,9 @@ def test_db_conn():
     conn=get_db_connection()
     
 
-if __name__=="__main__":
-    logging.basicConfig(filename=CONST.LOGGER_FILE_NAME, level=logging.DEBUG)
-    logging.warn(config.getint('S','a'))
-    print(config.getint('S','a'))
+def testSuite1():
     d=DAO()
-    dl=DAOList(DAO())
+    dl=DAOList(DAO)
     dl.add(d)
     try:
         dl.add(None)
@@ -25,8 +23,7 @@ if __name__=="__main__":
         print(exc)
     dl.add(d)
     a=ADAO()
-    a.load()
-    adao_list=DAOList(a)
+    adao_list=DAOList(ADAO)
     adao_list.add(a)
     try:
         adao_list.add(d)
@@ -37,13 +34,41 @@ if __name__=="__main__":
     for a in adao_list:
         print("DAS IST A, sicher!!!: %s" % a)
     print("uuid: %s" % get_uuid_from_database())
+    uuid_to_load=None
     try:
         a1=ADAO()
         a1.a="test"
         print("vor dem save")
         a1.save()
+        uuid_to_load=a1.uuid()
         print("saved")
     except BaseException as ex:
         print(ex)
-    a2=ADAO("41cc8197-9643-4a0f-8b9d-0881356411ff")
+    a2=ADAO(uuid_to_load)
     print(a2)
+    for dao in adao_list:
+        print("delete first adao record from the list: %s" % dao.uuid)
+        dao.delete()
+        print("done!")
+        break    
+    
+if __name__=="__main__":
+    logging.basicConfig(filename=CONST.LOGGER_FILE_NAME, level=logging.DEBUG)
+    logging.warn(config.getint('S','a'))
+    print(config.getint('S','a'))
+    testSuite1()
+    
+    adao_list=DAOList(ADAO)
+    adao_list.load()
+    adao=None
+
+    for a in adao_list:
+        adao=a
+        adao.load()
+        break
+    
+    print(adao)
+    b=BDAO()
+    b.b="hier ist b!"
+    adao.addBDAO(b)
+    print(adao)
